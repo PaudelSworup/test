62dbc8f2208f8801bc6f3c11ed9b2649e4ccf862
paudelSworup <sworupkc9@gmail.com>
2025-03-07 13:43:42 +0545

    new pipeline

--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -1,4 +1,7 @@
 import express, { Request, RequestHandler, Response } from "express";
 export const Demo = async (req: Request, res: Response) => {
-  res.status(200).json({ success: true, messsage: "successful" });
+  res.status(200).json({
+    success: true,
+    messsage: "successfully executed with cicd pielines using github actions",
+  });
 };
